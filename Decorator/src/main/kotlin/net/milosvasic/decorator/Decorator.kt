1fb1cdce1bcdba9be3fcfcf9952bca70a74c9236
Milos Vasic <milos85vasic@gmail.com>
2017-04-01 21:00:02 +0200

    Decorator: Writing template parser.

--- Decorator/src/main/kotlin/net/milosvasic/decorator/Decorator.kt
+++ Decorator/src/main/kotlin/net/milosvasic/decorator/Decorator.kt
@@ -2,6 +2,8 @@ package net.milosvasic.decorator
 
 interface Decorator {
 
+    val openingTag : String
+    val closingTag : String
     val templateExtension : String
 
     fun decorate(template: String): String
--- Decorator/src/main/kotlin/net/milosvasic/decorator/DecoratorImpl.kt
+++ Decorator/src/main/kotlin/net/milosvasic/decorator/DecoratorImpl.kt
@@ -1,13 +1,34 @@
 package net.milosvasic.decorator
 
+import java.util.regex.Pattern
+
 class DecoratorImpl : Decorator {
 
+    override val openingTag: String = "<dc>"
+    override val closingTag: String = "</dc>"
     override val templateExtension = "decorator"
 
     override fun decorate(template: String): String {
         val templateFile = javaClass.classLoader.getResource("$template.$templateExtension")
+        var rendered = StringBuilder()
         val content = templateFile.readText()
-        return content
+        val rows = content.split("\n")
+        val decoratedRows = mutableMapOf<Int, String>()
+        rows.forEachIndexed {
+            index, line ->
+            val p = Pattern.compile("$openingTag(.+?)$closingTag")
+            val m = p.matcher(line)
+            while (m.find()) {
+                val result = m
+                        .group()
+                        .replace(openingTag, "")
+                        .replace(closingTag, "")
+                        .trim()
+                decoratedRows[index] = result
+                println(result)
+            }
+        }
+        return rendered.toString()
     }
 
 }
