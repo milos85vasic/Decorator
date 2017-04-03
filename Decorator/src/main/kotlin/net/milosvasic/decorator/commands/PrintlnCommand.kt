d18174f7f0f0d417bfab266360a8b36966e2a7f0
Milos Vasic <milos85vasic@gmail.com>
2017-04-03 09:00:09 +0200

    Decorator: Working on println  method.

--- Decorator/src/main/kotlin/net/milosvasic/decorator/commands/PrintlnCommand.kt
+++ Decorator/src/main/kotlin/net/milosvasic/decorator/commands/PrintlnCommand.kt
@@ -9,7 +9,16 @@ class PrintlnCommand : TemplateCommand() {
 
     override fun invoke(parameters: List<String>): ContentResult {
         checkParameters(parameters)
-        return ContentResult(parameters[0])
+        var toPrint = parameters[0]
+        if (toPrint.startsWith("\"")) {
+            toPrint = toPrint.substring(1, toPrint.lastIndex)
+        }
+        if (toPrint.endsWith("\"")) {
+            toPrint = toPrint.substring(toPrint.lastIndex - 1, toPrint.lastIndex)
+        }
+//        parameters[0]
+//                .split(" ")
+        return ContentResult(toPrint)
     }
 
 }
