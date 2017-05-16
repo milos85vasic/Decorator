a5ebcd0450ebff9507838b16d8de92fe6f5f9fd3
Milos Vasic <milos85vasic@gmail.com>
2017-05-16 08:30:38 +0200

    Decorator: Data builder.

--- Decorator/src/main/kotlin/net/milosvasic/decorator/data/DataBuilder.kt
+++ Decorator/src/main/kotlin/net/milosvasic/decorator/data/DataBuilder.kt
@@ -2,9 +2,22 @@ package net.milosvasic.decorator.data
 
 class DataBuilder {
 
-    // TODO: Write implementation for builder.
-
     private val data = Data()
+
+    fun append(key: String, value: String): DataBuilder {
+        data.content[key] = Value(value)
+        return this
+    }
+
+    fun append(key: String, value: Value): DataBuilder {
+        data.content[key] = value
+        return this
+    }
+
+    fun append(key: String, data: Data): DataBuilder {
+        data.content[key] = data
+        return this
+    }
 
     fun build(): Data {
         return data
