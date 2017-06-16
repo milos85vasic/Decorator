a70bfc0fc3355ddede8753b22b7ca70ebf93f344
Milos Vasic <milos85vasic@gmail.com>
2017-06-16 09:19:37 +0200

    Decorator: 'For' - working on test assertions.

--- Decorator/src/test/kotlin/net/milosvasic/decorator/ForeachTest.kt
+++ Decorator/src/test/kotlin/net/milosvasic/decorator/ForeachTest.kt
@@ -123,7 +123,134 @@ Zzz
 """<p>
 Name: John
 Last name: Smith
-</p>"""
+</p>""",
+
+"""<ul>  ok
+    <li>
+        <a href="/0">
+            Index [ 0 ]
+            Item [ First ]
+        </a>
+    </li>
+    <li>
+        <a href="/1">
+            Index [ 1 ]
+            Item [ Second ]
+        </a>
+    </li>
+    <li>
+        <a href="/2">
+            Index [ 2 ]
+            Item [ Third ]
+        </a>
+    </li>
+</ul>
+---""",
+
+
+"""<ul>  ok
+    <li>
+        <a href="/0">
+            Worker:
+            Index [ 0 ]
+            First name: [ John ]
+            Last name: [ Smith ]
+        </a>
+    </li>
+    <li>
+        <a href="/1">
+            Worker:
+            Index [ 1 ]
+            First name: [ Pera ]
+            Last name: [ Peric ]
+        </a>
+    </li>
+    <li>
+        <a href="/2">
+            Worker:
+            Index [ 2 ]
+            First name: [ Unknown ]
+            Last name: [ - - - ]
+        </a>
+    </li>
+</ul>""",
+
+"""---
+<ul>
+    <li>
+        <a href="/">
+            0
+            00000
+            0
+            0 dsdsds 0 kssksks 0 dkdd
+        </a>
+    </li>
+    <li>
+        <a href="/">
+            1
+            11111
+            1
+            1 dsdsds 1 kssksks 1 dkdd
+        </a>
+    </li>
+    <li>
+        <a href="/">
+            2
+            22222
+            2
+            2 dsdsds 2 kssksks 2 dkdd
+        </a>
+    </li>
+</ul>
+---""",
+
+"""<ul>
+ dd
+    <li>
+        <a href="/0">Item [ 0 ][ Xxx ]</a>
+    </li>
+ dd
+    <li>
+        <a href="/1">Item [ 1 ][ Yyy ]</a>
+    </li>
+ dd
+    <li>
+        <a href="/2">Item [ 2 ][ Zzz ]</a>
+    </li>
+ dd
+    <li>
+        <a href="/3">Item [ 3 ][ 111 ]</a>
+    </li>
+ dd
+    <li>
+        <a href="/4">Item [ 4 ][ 222 ]</a>
+    </li>
+</ul>
+---
+<ul>
+</ul>
+---""",
+
+"""<p>Footer text: f.o.o.t.e.r.</p>
+---
+<ul>
+    <li>
+        <p>Footer text: f.o.o.t.e.r.</p>
+    </li>
+    <li>
+        <p>Footer text: f.o.o.t.e.r.</p>
+    </li>
+    <li>
+        <p>Footer text: f.o.o.t.e.r.</p>
+    </li>
+    <li>
+        <p>Footer text: f.o.o.t.e.r.</p>
+    </li>
+    <li>
+        <p>Footer text: f.o.o.t.e.r.</p>
+    </li>
+</ul>
+---"""
 
         )
 
