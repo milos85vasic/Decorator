0916e62113ff13d89b2b1af0de10ee1a22018abf
Milos Vasic <milos85vasic@gmail.com>
2017-06-18 10:55:11 +0200

    Decorator: Decorator main test - work in progress: Data for the test.

--- Decorator/src/test/kotlin/net/milosvasic/decorator/MainTest.kt
+++ Decorator/src/test/kotlin/net/milosvasic/decorator/MainTest.kt
@@ -1,5 +1,6 @@
 package net.milosvasic.decorator
 
+import net.milosvasic.decorator.data.data
 import net.milosvasic.logger.SimpleLogger
 import net.milosvasic.logger.VariantsConfiguration
 import org.junit.Test
@@ -11,6 +12,72 @@ class MainTest {
 
     @Test
     fun testDecoratorCompletely() {
+        val data = data()
+                .append("company", "Umbrella Corporation")
+                .append("enterprise", "")
+                .append(
+                        "sections",
+                        data()
+                                .append("main", "Main section")
+                                .append("floor 1", "Floor 1")
+                                .append("floor 2", "Floor 2")
+                                .append("floor 3", "")
+                                .build()
+                )
+                .append("skills", listOf("social", "engineer", "cleaner", "cook"))
+                .append(
+                        "subskills",
+                        data()
+                                .append("social", listOf("talking", "leading"))
+                                .build()
+                )
+                .append(
+                        "subskills",
+                        data()
+                                .append("social", listOf("talking shadowed", "leading shadowed"))
+                                .build()
+                )
+                .append(
+                        "education_levels",
+                        data()
+                                .append("high_school", "")
+                                .build()
+                )
+                .append(
+                        "workers",
+                        listOf(
+                                data()
+                                        .append("firstName", "John")
+                                        .append("lastName", "Smith")
+                                        .append(
+                                                "skills",
+                                                data().append(
+                                                        "social", "excellent"
+                                                )
+                                        )
+                                        .build(),
+                                data()
+                                        .append("firstName", "Pera")
+                                        .append("lastName", "Peric")
+                                        .build(),
+                                data()
+                                        .append("firstName", "Unknown")
+                                        .build(),
+                                data()
+                                        .append("firstName", "John")
+                                        .append("lastName", "Smith 2nd")
+                                        .append(
+                                                "skills",
+                                                data()
+                                                        .append("social", "fair")
+                                                        .append("engineer", "excelent")
+                                        )
+                                        .build()
+                        )
+                )
+                .append("coworkers", data().build())
+                .build()
+
 
     }
 
